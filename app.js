3fa71917ebc1e7d0ae6b7aaebfe7b2c61065e62e
venkatnarasimha <venkatkommineni9@gmail.com>
2021-02-24 14:26:32 +0530

    port => PORT

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const api = require('./api');
 
 app.use(express.json());
