3a642d5c031ebc57cf81a99f6e7e04ad6d84ca22
venkatnarasimha <venkatkommineni9@gmail.com>
2021-02-24 12:06:21 +0530

    changed majority of things on client side

---
+++ public/shorten.js
@@ -0,0 +1,75 @@
+/* eslint-disable no-console */
+const inputValue = document.querySelector('.input');
+const outputValue = document.querySelector('.output');
+const copy=document.querySelector('.copy');
+const but = document.querySelector('.shortens');
+const mylist = document.querySelector('.myurl');
+const short=document.querySelector('.shorturl');
+function logic() {
+    fetch('http://localhost:3000/api/urls', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+        body: JSON.stringify({ long_url: inputValue.value }),
+    })
+        .then((response) => response.json())
+        .then((json) => {
+            // outputValue.style.display='inline-block';
+            document.querySelector('.output').value =`${window.location.origin}/${json.id}`;
+        })
+        .catch((err) => console.log(`err :${err}`));
+}
+
+
+function displayURLS() {
+    fetch('http://localhost:3000/api/urls')
+    .then((response) => response.json())
+    .then((urls) => {
+            urls.forEach(element => {
+            console.log(element.long_url);
+            const longUrl=document.createTextNode(element.long_url);
+            console.log(longUrl);
+            const shortUrl=document.createTextNode(element.id);
+            const br=document.createElement('br');
+            const short=document.querySelector('.shorturl');
+            const long=document.querySelector('.longurl');
+            short.appendChild(shortUrl);
+            short.appendChild(br.cloneNode(true));
+            long.appendChild(longUrl);
+            long.appendChild(br.cloneNode(true));
+           
+            });
+
+        
+    });
+}
+
+but.addEventListener('click', () => {
+    console.log('clicked');
+    logic();
+    displayURLS();
+});
+
+// mylist.addEventListener('click', ()=>{
+//     displayURLS();
+// })
+
+copy.addEventListener('click',()=>{
+    const cop = document.querySelector('.output');
+    console.log("clicked",cop);
+    cop.focus();
+    cop.select();
+    document.execCommand('copy');
+    try{
+        const successful=document.execCommand('copy');
+        const msg=successful?'successful':'unseccessful';
+        console.log(`msg`);
+    }
+    catch(err) {
+            console.log('oops');
+    }
+});
+
+
+
